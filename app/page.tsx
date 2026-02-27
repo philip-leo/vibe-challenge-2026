b2a882dfca3da31eee5cb1ceca806e1ada1218d0
philip-leo <philip.l.winkler@gmail.com>
2026-02-27 11:05:42 +0100

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "100vh",
+      margin: 0,
+      fontFamily: "system-ui, -apple-system, sans-serif",
+      background: "#000",
+      color: "#fff",
+    }}>
+      <h1 style={{ fontSize: "3rem", fontWeight: 300 }}>Hello World</h1>
+    </div>
+  );
+}
